a5fbd4a17d844c33a51662e9a068985a81b03213
Y3llow45 <hristovborislav45@gmail.com>
2025-07-01 11:58:22 +0300

    Add EmployeeService and EmployeeController

---
+++ backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.EmployeeDto;
+
+public class EmployeeService {
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        return employeeDto;
+    }
+}
