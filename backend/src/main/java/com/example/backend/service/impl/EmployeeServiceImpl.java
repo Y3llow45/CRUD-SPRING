91b21f6492e6a3bc7bdbe56426e35da28fdf10a1
Y3llow45 <hristovborislav45@gmail.com>
2025-07-01 11:58:01 +0300

    Add EmployeeServiceImpl and override create method

---
+++ backend/src/main/java/com/example/backend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.backend.service.impl;
+
+import com.example.backend.dto.EmployeeDto;
+import com.example.backend.entity.Employee;
+import com.example.backend.mapper.EmployeeMapper;
+import com.example.backend.repository.EmployeeRepository;
+import com.example.backend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+}
