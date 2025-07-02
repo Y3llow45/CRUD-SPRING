0b0c902539a438a9c3fe551753c966e8f1e2cb42
Y3llow45 <hristovborislav45@gmail.com>
2025-07-02 12:19:32 +0300

    Fix null id when creating new employee

--- backend/src/main/java/com/example/backend/service/EmployeeService.java
+++ backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -2,8 +2,6 @@ package com.example.backend.service;
 
 import com.example.backend.dto.EmployeeDto;
 
-public class EmployeeService {
-    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return employeeDto;
-    }
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
 }
